d4d28bd163f91770db4f091e1de2ec2834d8e155
thesunfei <thesunfei@hotmail.com>
2022-05-24 15:53:20 +0800

    v1.1.0, add instance class.

--- index.js
+++ index.js
@@ -1,77 +1,114 @@
 import vaxue from "vaxue"
+let put = function (requestOptions, IDBOptions = {}, processOptions = {}) {
+    if (arguments.length == 0) {
+        throw new Error("Failed to execute 'put': 1 argument required, but only 0 present.")
+    }
+    if (typeof requestOptions == "string") {
+        requestOptions = {
+            url: requestOptions
+        }
+    }
+    return new Promise((resolve, reject) => {
+        let request = indexedDB.open(IDBOptions.dbName || "EasyIDB", IDBOptions.version || 1);
+        request.onupgradeneeded = (e) => {
+            e.target.result.createObjectStore(IDBOptions.storeName || "cache");
+        };
+        request.onsuccess = (e) => {
+            let database = e.target.result;
+            try {
+                var transaction = database.transaction([IDBOptions.storeName || "cache"], "readonly");
+                let store = transaction.objectStore(IDBOptions.storeName || "cache");
+                store.openCursor(requestOptions.url).onsuccess = async (e) => {
+                    let cursor = e.target.result;
+                    if (cursor) {
+                        let blob = cursor.value;
+                        if (processOptions.processBlob) {
+                            blob = await processOptions.processBlob(blob)
+                        }
+                        resolve({
+                            blob,
+                            get url() {
+                                return URL.createObjectURL(blob)
+                            }
+                        });
+                    } else {
+                        let blob = await vaxue.get({
+                            ...requestOptions,
+                            responseType: "blob"
+                        });
+                        if (processOptions.processBlob) {
+                            blob = await processOptions.processBlob(blob)
+                        }
+                        transaction = database.transaction([IDBOptions.storeName || "cache"], "readwrite");
+                        store = transaction.objectStore(IDBOptions.storeName || "cache");
+                        store.put(blob, requestOptions.url);
+                        resolve({
+                            blob,
+                            get url() {
+                                return URL.createObjectURL(blob)
+                            }
+                        });
+                    }
+                }
+
+            } catch (e) {
+                console.error(e)
+                reject(e);
+            }
+        }
+    })
+};
+let get = function (url, IDBOptions = {}, processOptions = {}) {
+    return new Promise((resolve, reject) => {
+        let request = indexedDB.open(IDBOptions.dbName || "EasyIDB", IDBOptions.version || 1);
+        request.onsuccess = (e) => {
+            try {
+                let database = e.target.result;
+                var transaction = database.transaction([IDBOptions.storeName || "cache"], "readonly");
+                let store = transaction.objectStore(IDBOptions.storeName || "cache");
+                store.get(url).onsuccess = async (e) => {
+                    if (e.target.result) {
+                        let blob = e.target.result;
+                        if (processOptions.processBlob) {
+                            blob = await processOptions.processBlob(blob)
+                        }
+                        resolve({
+                            blob,
+                            get url() {
+                                return URL.createObjectURL(e.target.result)
+                            }
+                        });
+                    } else {
+                        resolve(false)
+                    }
+                }
+            } catch (e) {
+                console.error(e)
+                resolve(false)
+            }
+        }
+    })
+}
 export default {
-    put(requestOptions, IDBOptions = {}) {
+    put,
+    get,
+    instance: function (requestOptions, IDBOptions = {}, processOptions = {}) {
         if (typeof requestOptions == "string") {
             requestOptions = {
                 url: requestOptions
             }
         }
-        return new Promise(async (resolve, reject) => {
-            let request = indexedDB.open(IDBOptions.dbName || "EasyIDB", IDBOptions.version || 1);
-            request.onupgradeneeded = (e) => {
-                e.target.result.createObjectStore(IDBOptions.storeName || "cache");
-            };
-            request.onsuccess = function (e) {
-                let database = e.target.result;
-                try {
-                    var transaction = database.transaction([IDBOptions.storeName || "cache"], "readonly");
-                    let store = transaction.objectStore(IDBOptions.storeName || "cache");
-                    store.openCursor(requestOptions.url).onsuccess = async function (e) {
-                        let cursor = e.target.result;
-                        if (cursor) {
-                            resolve({
-                                blob: cursor.value,
-                                get url() {
-                                    return URL.createObjectURL(cursor.value)
-                                }
-                            });
-                        } else {
-                            let blob = await vaxue.get({
-                                ...requestOptions,
-                                responseType: "blob"
-                            });
-                            transaction = database.transaction([IDBOptions.storeName || "cache"], "readwrite");
-                            store = transaction.objectStore(IDBOptions.storeName || "cache");
-                            store.put(blob, requestOptions.url);
-                            resolve({
-                                blob,
-                                get url() {
-                                    return URL.createObjectURL(blob)
-                                }
-                            });
-                        }
-                    }
-
-                } catch (e) {
-                    reject(e);
-                }
-            }
-        })
-    },
-    get(url, IDBOptions = {}) {
-        return new Promise((resolve, reject) => {
-            let request = indexedDB.open(IDBOptions.dbName || "EasyIDB", IDBOptions.version || 1);
-            request.onsuccess = function (e) {
-                try {
-                    let database = e.target.result;
-                    var transaction = database.transaction([IDBOptions.storeName || "cache"], "readonly");
-                    let store = transaction.objectStore(IDBOptions.storeName || "cache");
-                    store.get(url).onsuccess = function (e) {
-                        if (e.target.result) {
-                            resolve({
-                                blob: e.target.result,
-                                get url() {
-                                    return URL.createObjectURL(e.target.result)
-                                }
-                            });
-                        } else {
-                            resolve(false)
-                        }
-                    }
-                } catch (e) {
-                    resolve(false)
-                }
-            }
-        })
+        this.put = async (url) => {
+            return await put({
+                ...requestOptions,
+                url
+            }, IDBOptions, processOptions);
+        }
+        this.get = async (url) => {
+            return await get({
+                ...requestOptions,
+                url
+            }, IDBOptions, processOptions);
+        }
     }
 }
--- readme.md
+++ readme.md
@@ -22,13 +22,14 @@ let process=async function(){
 ```
 # API
 
-## IDB.put(requestOptions, IDBOptions)
+## IDB.put(requestOptions, IDBOptions, processOptions)
 
 The put() method of the IDB Object inserts a new record if the given item doesn't exist.
 It always return a promise that resolves to an object which contains the blob object of 
 the requested resource and blob URL of it if no error happened.
+If the requested item already stored in database, a blob URL of it will be directly generated, no request to the server would be made.
 
-### requestOptions
+### requestOptions (required)
 Config options of ajax request. See 
 https://github.com/thesunfei/vaxue/blob/master/readme.md#config-options 
 for this usage.
@@ -42,7 +43,36 @@ for this usage.
 }
 ```
 
-## IDB.get(url, IDBOptions)
+### processOptions
+processOptions.processBlob can be used to modify requested data.
+```js
+{
+    processBlob: async function processSVG(blob) {//This is an example that can automatically add width and height attributes to svg
+        if (!(blob.type == "image/svg+xml")) return blob;
+        let svgString = await blob.text();
+        let parser = new DOMParser();
+        let svgDom = parser.parseFromString(svgString, "image/svg+xml").documentElement;
+        let viewBox = (svgDom.getAttribute("viewBox") || "0 0 300 300").replace(/  +/g, " ").split(" ");
+        let width = viewBox[2];
+        let height = viewBox[3];
+        svgDom.setAttribute("width", width);
+        svgDom.setAttribute("height", height);
+        let svgStr = svgDom.outerHTML;
+        return new Blob([svgStr], {
+            type: "image/svg+xml"
+        })
+    }
+}
+```
+
+## IDB.get(url, IDBOptions, processOptions)
 The get() method of the IDB Object only return a promise that resolves to an object which contains 
 the blob object of the requested resource and blob URL of it if the 
 specified resource already exsist in the database otherwise it returns false.
+
+## IDB.instance(requestOptions, IDBOptions, processOptions)
+
+```js
+let IDBInstance = new IDB.instance({headers:{Authorization:"0d31eda111de"}},{dbName:'EasyIDB',storeName:'cache',version:1},{processBlob:function(blob){return blob}});
+IDBInstance.put("/example.svg").then(res=>{console.log(res.url,res.blob)});
+```
